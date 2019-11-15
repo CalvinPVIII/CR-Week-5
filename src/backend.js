2646041af089e1a1551653a3d34e57a56dcaffa7
unknown <calvinp003@gmail.com>
2019-11-15 12:47:43 -0800

    refactor prototypes to combine all previous prototypes into one, change test to reflect refactoring

--- src/backend.js
+++ src/backend.js
@@ -14,31 +14,14 @@ export class Person {
   }
 }
 
-Person.prototype.findDaysAlive = function () {
+Person.prototype.findDaysAliveAndAgeOnEachPlanetAndLifeExpectancyAndYearsLeftOnEachPlanet = function () {
   this.days = Math.round((Date.now() - this.birthday.getTime()) / 8.64e+7);
-};
-
-Person.prototype.findMercuryYears = function () {
   this.mercuryYears = (this.days / 88).toFixed(1);
-};
-
-Person.prototype.findVenusYears = function () {
   this.venusYears = (this.days / 225).toFixed(1);
-};
-
-Person.prototype.findMarsYears = function () {
   this.marsYears = (this.days / 687).toFixed(1);
-};
-
-Person.prototype.findJupiterYears = function () {
   this.jupiterYears = (this.days / 4300).toFixed(1);
-};
-
-Person.prototype.getLifeExpectancy = function () {
   this.lifeExpectancy = this.gender + this.continent
-}
-
-Person.prototype.howManyYearsLeft = function () {
   let daysLeft = (this.lifeExpectancy * 365) - this.days;
   this.yearsLeft = [(daysLeft/365).toFixed(1), (daysLeft/88).toFixed(1), (daysLeft/225).toFixed(1), (daysLeft/687).toFixed(1), (daysLeft/4300).toFixed(1)]
-  }
+
+}
--- src/main.js
+++ src/main.js
@@ -2,13 +2,16 @@ import $ from 'jquery'
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './styles.css';
-import { Person } from './../src/backend.js'
+import {
+  Person
+} from './../src/backend.js'
 
-$(document).ready(function(){
-$("#userInfo").submit(function(e){
-  e.preventDefault();
-  let userBirthdate = new Date (`${$("#month").val()} ${$("#day").val()} ${$("#year").val()}`)
-  let userInformation = new Person (userBirthdate, parseInt($("#continent").val()),parseInt($("#gender").val()))
-  console.log(userInformation);
-})
+$(document).ready(function() {
+  $("#userInfo").submit(function(e) {
+    e.preventDefault();
+    let userBirthdate = new Date(`${$("#month").val()} ${$("#day").val()} ${$("#year").val()}`)
+    let userInformation = new Person(userBirthdate, parseInt($("#continent").val()), parseInt($("#gender").val()))
+    userInformation.findDaysAliveAndAgeOnEachPlanetAndLifeExpectancyAndYearsLeftOnEachPlanet();
+
+  });
 });
