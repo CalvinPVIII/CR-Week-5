e3dc9f49568e818abec358889f6ae3299b740bac
unknown <calvinp003@gmail.com>
2019-11-15 09:05:11 -0800

    write findDaysAlive code for test

--- src/backend.js
+++ src/backend.js
@@ -1,15 +1,17 @@
 class Person {
   constructor(birthday) {
-    this.birthday = birthday
-    this.days = 0
-    this.mercuryYears = 0
-    this.venusYears = 0
-    this.marsYears = 0
+    this.birthday = birthday;
+    this.days = 0;
+    this.mercuryYears = 0;
+    this.venusYears = 0;
+    this.marsYears = 0;
     this.jupiterYears = 0
 
   }
 }
 
 Person.prototype.findDaysAlive = function () {
+  let milliseconds = date.now() - this.birthday.getTime();
+  this.days = Math.round(milliseconds / 8.64e+7);
 
 };
