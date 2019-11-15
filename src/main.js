5576d270282b75f08feb994dc59e249b299af7c3
unknown <calvinp003@gmail.com>
2019-11-15 13:37:06 -0800

    add ui function that will show and hide information based on inputted age

--- src/main.js
+++ src/main.js
@@ -6,12 +6,42 @@ import {
   Person
 } from './../src/backend.js'
 
+let uiChanger = function (personObject) {
+  $("#daysOld").text(personObject.days)
+  $("#mercuryYears").text(personObject.mercuryYears)
+  $("#venusYears").text(personObject.venusYears)
+  $("#marsYears").text(personObject.marsYears)
+  $("#jupiterYears").text(personObject.jupiterYears)
+  if (personObject.yearsLeft[0] > 0) {
+    $(".result-younger-expectancy").show();
+    $(".result-older-expectancy").hide();
+    $("#earthLeft").text(personObject.yearsLeft[0])
+    $("#mercuryLeft").text(personObject.yearsLeft[1])
+    $("#venusLeft").text(personObject.yearsLeft[2])
+    $("#marsLeft").text(personObject.yearsLeft[3])
+    $("#jupiterLeft").text(personObject.yearsLeft[4])
+
+  } else if (personObject.yearsLeft[0] < 1){
+    $(".result-older-expectancy").show();
+    $(".result-younger-expectancy").hide();
+    for (let i = 0; i < personObject.yearsLeft.length; i++) {
+      personObject.yearsLeft[i] *= -1
+    }
+    $("#earthExtra").text(personObject.yearsLeft[0])
+    $("#mercuryExtra").text(personObject.yearsLeft[1])
+    $("#venusExtra").text(personObject.yearsLeft[2])
+    $("#marsExtra").text(personObject.yearsLeft[3])
+    $("#jupiterExtra").text(personObject.yearsLeft[4])
+  }
+  $(".result-age-on-planets").show();
+}
+
 $(document).ready(function() {
   $("#userInfo").submit(function(e) {
     e.preventDefault();
     let userBirthdate = new Date(`${$("#month").val()} ${$("#day").val()} ${$("#year").val()}`)
     let userInformation = new Person(userBirthdate, parseInt($("#continent").val()), parseInt($("#gender").val()))
     userInformation.findDaysAliveAndAgeOnEachPlanetAndLifeExpectancyAndYearsLeftOnEachPlanet();
-
+    uiChanger(userInformation);
   });
 });
