489059e72ee8554f3c77567e30830bccfaa8d55c
unknown <calvinp003@gmail.com>
2019-11-16 16:56:39 -0800

    change styling

--- src/main.js
+++ src/main.js
@@ -7,35 +7,45 @@ import {
 } from './../src/backend.js';
 
 
-let uiChanger = function(personObject) {
-  $("#daysOld").text(personObject.days);
-  $("#mercuryYears").text(personObject.mercuryYears);
-  $("#venusYears").text(personObject.venusYears);
-  $("#marsYears").text(personObject.marsYears);
-  $("#jupiterYears").text(personObject.jupiterYears);
-  if (personObject.yearsLeft[0] > 0) {
-    $(".result-younger-expectancy").show();
+function uiChanger(personObject) {
+  if (isNaN(personObject.days) === true) {
     $(".result-older-expectancy").hide();
-    $("#earthLeft").text(personObject.yearsLeft[0]);
-    $("#mercuryLeft").text(personObject.yearsLeft[1]);
-    $("#venusLeft").text(personObject.yearsLeft[2]);
-    $("#marsLeft").text(personObject.yearsLeft[3]);
-    $("#jupiterLeft").text(personObject.yearsLeft[4]);
-
-  } else if (personObject.yearsLeft[0] < 1) {
-    $(".result-older-expectancy").show();
     $(".result-younger-expectancy").hide();
-    for (let i = 0; i < personObject.yearsLeft.length; i++) {
-      personObject.yearsLeft[i] *= -1;
+    $(".error").show();
+
+  } else {
+    $("#daysOld").text(personObject.days);
+    $("#mercuryYears").text(personObject.mercuryYears);
+    $("#venusYears").text(personObject.venusYears);
+    $("#marsYears").text(personObject.marsYears);
+    $("#jupiterYears").text(personObject.jupiterYears);
+    if (personObject.yearsLeft[0] > 0) {
+      $(".error").hide();
+      $(".result-younger-expectancy").show();
+      $(".result-older-expectancy").hide();
+      $("#earthLeft").text(personObject.yearsLeft[0]);
+      $("#mercuryLeft").text(personObject.yearsLeft[1]);
+      $("#venusLeft").text(personObject.yearsLeft[2]);
+      $("#marsLeft").text(personObject.yearsLeft[3]);
+      $("#jupiterLeft").text(personObject.yearsLeft[4]);
+
+    } else if (personObject.yearsLeft[0] < 1) {
+      $(".error").hide();
+      $(".result-older-expectancy").show();
+      $(".result-younger-expectancy").hide();
+      for (let i = 0; i < personObject.yearsLeft.length; i++) {
+        personObject.yearsLeft[i] *= -1;
+      }
+      $("#earthExtra").text(personObject.yearsLeft[0]);
+      $("#mercuryExtra").text(personObject.yearsLeft[1]);
+      $("#venusExtra").text(personObject.yearsLeft[2]);
+      $("#marsExtra").text(personObject.yearsLeft[3]);
+      $("#jupiterExtra").text(personObject.yearsLeft[4]);
     }
-    $("#earthExtra").text(personObject.yearsLeft[0]);
-    $("#mercuryExtra").text(personObject.yearsLeft[1]);
-    $("#venusExtra").text(personObject.yearsLeft[2]);
-    $("#marsExtra").text(personObject.yearsLeft[3]);
-    $("#jupiterExtra").text(personObject.yearsLeft[4]);
+    $(".result-age-on-planets").show();
   }
-  $(".result-age-on-planets").show();
-};
+}
+
 
 $(document).ready(function() {
   $("#userInfo").submit(function(e) {
